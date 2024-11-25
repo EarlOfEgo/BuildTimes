fe62244578790fdf91a914cecc60cd23d0f03a97
Stephan Hagios <stephan.hagios@pm.me>
2024-11-25 11:17:30 +0100

    added notification if build takes longer than defined

--- src/main/kotlin/dev/hagios/buildtimes/services/BuildTimesService.kt
+++ src/main/kotlin/dev/hagios/buildtimes/services/BuildTimesService.kt
@@ -1,11 +1,15 @@
 package dev.hagios.buildtimes.services
 
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
+import dev.hagios.buildtimes.settings.BuildTimesSettings
 
 
 @Service(Service.Level.PROJECT)
@@ -14,9 +18,12 @@ import com.intellij.openapi.diagnostic.thisLogger
         Storage("BuildTimesService.xml")
     ]
 )
-class BuildTimesService : PersistentStateComponent<Statistics>, Disposable {
+class BuildTimesService(
+    private val project: Project
+) : PersistentStateComponent<Statistics>, Disposable {
 
     private var state = Statistics()
+    private val settings = BuildTimesSettings.getInstance(project)
 
     override fun getState(): Statistics {
         return state
@@ -28,6 +35,16 @@ class BuildTimesService : PersistentStateComponent<Statistics>, Disposable {
 
     fun addBuildTime(startTime: Long, endTime: Long, successful: Boolean) {
         state = state.copy(builds = state.builds + Build(startTime, endTime, successful))
+        val totalTime = endTime - startTime
+        if (totalTime > settings.notificationTime && settings.areNotificationsEnabled) {
+            NotificationGroupManager.getInstance()
+                .getNotificationGroup("BuildTimes Notifications")
+                .createNotification("Build (${totalTime}ms) takes longer than ${settings.notificationTime}ms.", NotificationType.WARNING)
+                .apply {
+                    setTitle("Build time")
+                }
+                .notify(project)
+        }
     }
 
     override fun dispose() {
